4df004d05a2278a877abf35d66598affb5d902e0
Your Name <bavarne@gmail.com>
2023-09-06 16:47:36 +0200

    hjll

---
+++ linkedlist.py
@@ -0,0 +1,6 @@
+ 
+#Q1. Write a program that finds and prints the minimum and maximum values in a singly linked list.
+# >> So you need to create a singly linked list and populate it with some data.
+# >> Write a function to find the min and max values.
+
+hhuhiunuin

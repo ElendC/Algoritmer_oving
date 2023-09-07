dfeb86c0ac7f0fd06fc96f089767a4f04e2e697e
Elend <elend@pop-os.localdomain>
2023-09-07 17:00:44 +0200

    oppgave 1 ferdig

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+import random
 
 class Node:
     def __init__(self, data):
@@ -9,24 +10,49 @@ class LinkedList:                       #tom linked list
         self.head = None                #første nodeobject
 
     
-    def addNode(self, data):            #Legger til ny node
+    def addNode(self, data):           
         new_node = Node(data)           #calling Node class to create a new node
         new_node.next = self.head       #Gjør førsde node om til neste
         self.head = new_node            #Setter nye node inn som første node
 
     def print_linked_list(self):
-        tempvar = self.head
-        while tempvar.next != None:
-            print(tempvar.data)
-            tempvar = tempvar.next
-        print(tempvar.data)
+        print("The Linked List:", end=" ")
+        TheLinkedList = []
+        cur_node = self.head
+        while cur_node != None:
+            TheLinkedList.append(str(cur_node.data))
+            cur_node = cur_node.next
+        print(" -> ".join(TheLinkedList))
 
-        
+
+    def max_value(self):
+        maxV = self.head.data            #Første node til max node
+        next_node = self.head.next       #Neste node
+        while next_node != None: 
+            if next_node.data > maxV:     
+                maxV = next_node.data    
+            next_node = next_node.next
+        print("Max value: ", maxV)
+
+    def min_value(self):
+        minV = self.head.data
+        next_node = self.head.next
+        while next_node != None:
+            if next_node.data < minV:
+                minV = next_node.data
+            next_node = next_node.next
+        print("Lowest value: ", minV)
+            
+
 list = LinkedList()
-list.addNode(2)
-list.addNode(3)
-list.addNode(4)
-list.addNode(5)
-list.addNode(6)
+
+for i in range(8):
+    i = random.randint(1, 99)
+    list.addNode(i)
+
+
+    
 list.print_linked_list()
+list.max_value()
+list.min_value()
 
